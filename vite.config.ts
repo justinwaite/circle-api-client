3299c6abce6f0e5bb9cd0494c310f90c06d7d6ad
Justin Waite <justindwaite@gmail.com>
2022-06-29 10:36:58 -0600

    fix(build): fix the artifact names

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
       name: 'circle-api-client',
       entry: './src/index.ts',
       formats: ['cjs', 'es'],
-      fileName: (format) => `circle-api-client.${format === 'es' ? 'mjs' : format}`,
+      fileName: (format) => `index.${format === 'es' ? 'mjs' : format}`,
     },
     outDir: './dist',
     rollupOptions: {
